50fbe351b58dc3798282df6388a560d9913f08b8
Scott Mitchell <scottmitchell2014@gmail.com>
2025-11-21 14:17:14 -0800

    much simpler mod "manager" and load same time as menu

--- ModManager/Plugin.cs
+++ ModManager/Plugin.cs
@@ -2,10 +2,11 @@
 using BepInEx.Bootstrap;
 using BepInEx.Logging;
 using HarmonyLib;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
 using SFKUILib;
+using SuperFantasyKingdom.TitleScreen;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 namespace ModManager
 {
@@ -19,9 +20,9 @@ namespace ModManager
         public const string PLUGIN_NAME = "SFK ModManager";
         public const string PLUGIN_VERSION = "1.0.0";
 
-        Dictionary<string, PluginInfo> loadedPlugins = Chainloader.PluginInfos;
-        private UIMenu modsMenu;
+        public static Dictionary<string, PluginInfo> loadedPlugins = Chainloader.PluginInfos;
 
+        bool triggered = false;
         private void Awake()
         {
             // Plugin startup logic
@@ -30,63 +31,28 @@ namespace ModManager
 
             Harmony harmony = new Harmony(PLUGIN_GUID);
             harmony.PatchAll();
-
-            SceneManager.sceneLoaded += OnSceneLoaded;
         }
 
-        private void CreateModButton()
+
+        public static void CreateModsListOverlay()
         {
-            var go = GameObject.Find("Canvas/Left/Menu");
+            var go = GameObject.Find("Canvas");
             Logger.LogInfo($"{go.name}");
             var transformGo = go.transform;
-
-            var profile = GameObject.Find("Canvas/Left/Menu/Profiles").GetComponent<RectTransform>();
-            var exit = GameObject.Find("Canvas/Left/Menu/Exit").GetComponent<RectTransform>();
-
-            var spacing = exit ? profile.anchoredPosition.y - exit.anchoredPosition.y : -60.0f;
-
-            var btn = UIButton.Create("Mods", transformGo, UIButton.STANDARD_SIZE, Vector2.zero, rounded: true, enableShake: true);
-            // Copy Exit's layout rules
-            var rect = btn.Rect;
-            rect.anchorMin = profile.anchorMin;
-            rect.anchorMax = profile.anchorMax;
-            rect.pivot = profile.pivot;
-
-            rect.sizeDelta = profile.sizeDelta;
-            rect.anchoredPosition = new Vector2(profile.anchoredPosition.x, profile.anchoredPosition.y + spacing);
-
-            // Insert after Exit
-            btn.Rect.SetSiblingIndex(profile.GetSiblingIndex() + 1);
-
-            btn.onClick(() => CreateModsListOverlay());
-        }
-
-        private void CreateModsListOverlay()
-        {
-            if (modsMenu == null)
+            int spacing = -35;
+            for (int i = 0; i < loadedPlugins.Count; i++)
             {
-                var mainCanvas = GameObject.Find("Canvas").transform;
-
-                modsMenu = UIMenu.Create(new Vector2(500, 600), new Vector2(-500, 0), mainCanvas, bgColor: Color.blue);
-                var row = modsMenu.AddVerticalLayout();
-                foreach (var plugin in loadedPlugins)
-                {
-                    row.AddButton(plugin.Value.Metadata.Name, new Vector2(325, 50), enableShake: true);
-                }
+                var text = UIText.Create(loadedPlugins.ElementAt(i).Value.Metadata.Name, transformGo, new Vector2(-700, 200 + (i + 1) * spacing), 18);
             }
-            modsMenu.Active = !modsMenu.Active;
-        }
-
-        void OnSceneLoaded(Scene scene, LoadSceneMode loadMode)
-        {
-            if (scene.name == "TitleScene")
-            {
-                CreateModButton();
-            } 
         }
 
         void Update()
         {
+            if (!triggered && TitleScreenManager.Instance.menu.activeSelf)
+            {
+                CreateModsListOverlay();
+                triggered = true;
+            }
            
         }
     }
