b675068268c9166aa76d54d1629a5e51a3791408
Scott Mitchell <scottmitchell2014@gmail.com>
2025-11-27 19:00:20 -0800

    new mod to add a new outpost

---
+++ NewBuildingTest/Patches/Patch_Buildings.cs
@@ -0,0 +1,128 @@
+﻿using HarmonyLib;
+using SuperFantasyKingdom;
+using SuperFantasyKingdom.Buildings;
+using SuperFantasyKingdom.UI;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NewBuildingTest
+{
+    // All this just to get another button
+    // Essentially replicating AddBuildingButtons
+    [HarmonyPatch(typeof(UIOverlayBuildingSelection), "AddBuildingButtons")]
+    static class Patch_AddBuildingButtons
+    {
+        static void Postfix(UIOverlayBuildingSelection __instance, Vector3 pos, BuildingSize size)
+        {
+            var my = Plugin.BigWood;
+            if (my == null) return;
+
+            var myCity = my.GetComponent<BuildingCity>();
+            if (myCity == null) return;
+
+            // only add for matching size/category and standard gating
+            if (size != myCity.GetBuildingSize()) return;
+            if (myCity.GetBuildingCategory() == BuildingCategory.None) return;
+            if (!CityManager.Instance.CanBuild(myCity.GetBuildingType())) return;
+
+            var cost = myCity.GetCost();
+            if (cost != null && cost.Length > 0 && cost[0].amount < 0) return;
+
+            // parent/category bucket
+            Transform parent = __instance.GetParent(myCity.GetBuildingCategory(), __instance.transform);
+            GameObject btn = Object.Instantiate(__instance.buttonTemplate, parent);
+
+            // ----- TEXT (force your custom text here no matter what localization does)
+            var nameTf = btn.transform.Find("TextName");
+            var descTf = btn.transform.Find("TextDescription");
+
+            if (nameTf != null)
+            {
+                var nameLoc = nameTf.GetComponent<UnityEngine.Localization.Components.LocalizeStringEvent>();
+                if (nameLoc != null) nameLoc.enabled = false;
+
+                var nameTMP = nameTf.GetComponent<TextMeshProUGUI>();
+                if (nameTMP != null) nameTMP.text = "Massive Wood";
+            }
+
+            if (descTf != null)
+            {
+                var descLoc = descTf.GetComponent<UnityEngine.Localization.Components.LocalizeStringEvent>();
+                if (descLoc != null) descLoc.enabled = false;
+
+                var descTMP = descTf.GetComponent<TextMeshProUGUI>();
+                if (descTMP != null) descTMP.text = "You get x10 wood!";
+            }
+
+            // ----- ICON / SIZE / COSTS (same as vanilla)
+            var imageTf = btn.transform.Find("Image");
+            int num = (size == BuildingSize.Small || size == BuildingSize.WorldBig) ? 32 : 24;
+
+            if (imageTf != null)
+            {
+                imageTf.GetComponent<RectTransform>().sizeDelta =
+                    myCity.icon.bounds.size * num;
+                imageTf.GetComponent<Image>().sprite = myCity.icon;
+            }
+
+            btn.SetActive(true);
+            UIOverlayBuildingSelection.DisplayCosts(cost, btn);
+
+            // ----- CLICK: set flag so build uses your prototype
+            var button = btn.GetComponent<Button>();
+            if (!myCity.CanBeBuilt())
+            {
+                button.interactable = false;
+                btn.GetComponent<CanvasGroup>().alpha = 0.7f;
+            }
+            else
+            {
+                button.onClick.AddListener(() =>
+                {
+                    Plugin.IsPlacingBigWood = true;
+                    __instance.ButtonClicked(pos, myCity);
+                    Plugin.IsPlacingBigWood = false;
+                });
+
+                GamepadManager.Instance.SetGameObjectToSelect(btn, false);
+            }
+        }
+    }
+    // We cloned "WorldWood" so replace it with our new prefab
+    [HarmonyPatch(typeof(CityManager), "GetBuilding", new[] { typeof(BuildingType) })]
+    static class Patch_CityManager_GetBuilding
+    {
+        static void Postfix(BuildingType type, ref BuildingCity __result)
+        {
+            if (!Plugin.IsPlacingBigWood) return;
+            if (type != BuildingType.WorldWood) return;
+            if (Plugin.BigWood == null) return;
+
+            __result = Plugin.BigWood;
+        }
+    }
+    // Override the rewards and ignore the Check/Fix functions which keep resetting it 
+    [HarmonyPatch(typeof(Reward), nameof(Reward.Check))]
+    static class Patch_Reward_Check_BigWood
+    {
+        static bool Prefix(Reward __instance, bool real, ref List<int> __result)
+        {
+            // only during your special build click
+            if (!Plugin.IsPlacingBigWood) return true;
+
+            var forced = new List<RewardOption>
+            {
+                new RewardOptionResource(ResourceType.Wood, 10) { probability = 100 }
+            };
+
+            __instance.m_Reward = forced;
+            __instance.m_DidCheck = true;
+
+            __result = null;   // caller ignores list for Spawn anyway
+            return false;      // skip original Check (and thus Fix)
+        }
+    }
+
+}
---
+++ NewBuildingTest/Plugin.cs
@@ -0,0 +1,92 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using SuperFantasyKingdom;
+using SuperFantasyKingdom.Buildings;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace NewBuildingTest
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+        public static BuildingOutpostReward BigWood;
+        public static bool IsPlacingBigWood = false;
+        Harmony _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+            SceneManager.sceneLoaded += onSceneLoaded;
+            _harmony.PatchAll();
+        }
+
+        private void WorldBuildingDetails()
+        {
+            //[Info: New Building Test] WorldWood | WorldBig | Outpost
+            //[Info: New Building Test] WorldWheat | WorldBig | Outpost
+            //[Info: New Building Test] WorldStone | WorldBig | Outpost
+            //[Info: New Building Test] WorldOre | WorldBig | Outpost
+            //[Info: New Building Test] WorldHunter | WorldBig | Outpost
+            //[Info: New Building Test] WorldShards | WorldBig | Outpost
+            //[Info: New Building Test] WorldFaith | WorldBig | Outpost
+            //[Info: New Building Test] WorldBarracks | WorldBig | Outpost
+            //[Info: New Building Test] WorldArchery | WorldBig | Outpost
+            //[Info: New Building Test] WorldMages | WorldBig | Outpost
+            //[Info: New Building Test] WorldSummoners | WorldBig | Outpost
+            //[Info: New Building Test] WorldDifficulty | WorldBig | Outpost
+            //[Info: New Building Test] WorldBanish | WorldBig | Outpost
+            //[Info: New Building Test] WorldCoins | WorldBig | Outpost
+            //[Info: New Building Test] WorldAppeal | WorldBig | Outpost
+            //[Info: New Building Test] WorldRelic | WorldBig | Outpost
+            //[Info: New Building Test] WorldUnit | WorldBig | Outpost
+            // Debug Stuff
+            var allBuilding = RaceManager.Instance.GetBuildings(BuildingSize.WorldBig);
+            BuildingCity woodOutpost = null;
+            foreach (var building in allBuilding)
+            {
+                var bCity = building.GetComponent<BuildingCity>();
+                var type = bCity.GetBuildingType();
+                if (type == BuildingType.WorldWood)
+                {
+                    woodOutpost = bCity;
+                }
+                var size = bCity.GetBuildingSize();
+                var category = bCity.GetBuildingCategory();
+                Logger.LogInfo($"{type} | {size} | {category}");
+            }
+            if (woodOutpost != null)
+            {
+                Logger.LogInfo($"{woodOutpost.GetType().FullName}");
+            }
+
+            // Clone wood outpost to make big wood
+            if (woodOutpost is BuildingOutpostReward r_Wood)
+            {
+                var clonedGO = Instantiate(r_Wood.gameObject);
+                clonedGO.name = "Mod_WorldWood_Big";
+                clonedGO.transform.position = new Vector3(99999, 99999, 99999);
+                clonedGO.hideFlags = HideFlags.HideAndDontSave;
+                DontDestroyOnLoad(clonedGO);
+
+                BigWood = clonedGO.GetComponent<BuildingOutpostReward>();
+
+                Logger.LogInfo("BigWood prototype created.");
+            }
+
+        }
+
+        private void onSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name == "GameScene")
+            {
+                WorldBuildingDetails();
+            }
+        }
+    }
+}
